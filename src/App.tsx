c0bf6e87b48672b88a010b573d44248b339aad00
Young wook Jeon <ela87@naver.com>
2022-02-28 13:30:33 +0900

    Refreshing a page -> Hit the endpoints to refresh token

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { Alert } from './components/alert/Alert';
 import Footer from './components/global/Footer';
 import Header from './components/global/Header';
 import PageRender from './PageRender';
+import { refreshToken } from './redux/actions/authAction';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // dispatch(getCategories());
+    // dispatch(getHomeBlogs());
+    dispatch(refreshToken());
+  }, [dispatch]);
+  
   return (
     <div className="container">
       <Router>
@@ -20,6 +30,6 @@ function App() {
       </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/redux/actions/authAction.ts
+++ src/redux/actions/authAction.ts
@@ -3,7 +3,7 @@ import { AUTH, IAuthType } from '../types/authTypes';
 import { ALERT, IAlertType } from '../types/alertTypes';
 import { IUserLogin, IUserRegister } from '../../utils/TypeScript';
 import { postAPI, getAPI } from '../../utils/FetchData';
-import { validRegister, validPhone } from '../../utils/Validators';
+import { validRegister } from '../../utils/Validators';
 import { checkTokenExp } from '../../utils/CheckTokenExp';
 
 export const login = (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
@@ -33,6 +33,22 @@ export const register = (userRegister: IUserRegister) => async (dispatch: Dispat
     const res = await postAPI(`auth/register`, userRegister);
 
     dispatch({ type: ALERT, payload: { success: res.data.msg }});
+  } catch (err: any) {
+    dispatch({ type: ALERT, payload: { errors: err.response.data.msg }});
+  }
+}
+
+export const refreshToken = () => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+  const logged = localStorage.getItem('refresh');
+  if (logged !== 'ok') return;
+
+  try {
+    dispatch({ type: ALERT, payload: { loading: true }});
+    const res = await getAPI(`auth/refreshToken`);
+
+    dispatch({ type: AUTH, payload: res.data })
+
+    dispatch({ type: ALERT, payload: {} });
   } catch (err: any) {
     dispatch({ type: ALERT, payload: { errors: err.response.data.msg }});
   }
